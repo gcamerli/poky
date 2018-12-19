12ec6892237bd122622c0dd5dfcb2c35cd28b9fd
Gius. Camerlingo <gcamerli@protonmail.com>
2018-12-19 20:51:31 +0100

    Update run script | Add device /dev/net/tun

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # ./run.sh
 
 # To mount a USB device add the flag --device=/dev/ttyUSB0
-docker run -it --rm -h poky -v poky:/home/yocto/poky --name poky gcamerli/poky:latest
+docker run -it --rm -h poky -v poky:/home/yocto/poky --device=/dev/net/tun --name poky gcamerli/poky:latest
