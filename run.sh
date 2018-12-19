5d58b8a4c5b15ddec5758d3d0b617940c64e766b
Gius. Camerlingo <gcamerli@protonmail.com>
2018-12-19 23:45:29 +0100

    Clean run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # ./run.sh
 
 # To mount a USB device add the flag --device=/dev/ttyUSB0
-docker run -it --rm -h poky --network yocto \
+docker run -it -h poky --network yocto \
 	-v poky:/home/yocto/poky \
 	--cap-add=NET_ADMIN \
 	--device=/dev/net/tun \
