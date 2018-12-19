fdd3164c5a0ef8228dcda9407ac0c0364a99fc64
Gius. Camerlingo <gcamerli@protonmail.com>
2018-12-19 21:49:51 +0100

    Clean build script

--- build-install-dumb-init.sh
+++ build-install-dumb-init.sh
@@ -3,18 +3,9 @@
 builddir=`mktemp -d` || exit 1
 cd $builddir || exit 1
 
-if grep -q CentOS /etc/*release; then
-    INSTALL_CMD="yum -y install python34-pip glibc-static"
-    REMOVE_CMD="yum -y remove python34-pip glibc-static"
-elif grep -q Fedora /etc/*release; then
+if grep -q Fedora /etc/*release; then
     INSTALL_CMD="dnf -y install glibc-static"
     REMOVE_CMD="dnf -y remove glibc-static"
-elif grep -q Ubuntu /etc/*release || grep -q Debian /etc/*release; then
-    INSTALL_CMD="apt-get install -y python3-pip"
-    REMOVE_CMD="apt-get remove -y python3-pip"
-elif grep -q openSUSE /etc/*release; then
-    INSTALL_CMD="zypper --non-interactive install python3-pip glibc-devel-static"
-    REMOVE_CMD="zypper --non-interactive remove python3-pip glibc-devel-static"
 else
     exit 1
 fi
