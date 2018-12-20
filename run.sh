378900e1a6d245bdd72270b0ee762b2bb3eaf6bf
Gius. Camerlingo <gcamerli@protonmail.com>
2018-12-20 02:55:26 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 # To mount a USB device add the flag --device=/dev/ttyUSB0
 docker run -it --rm -h poky --network yocto \
 	-v poky:/home/yocto/poky \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=unix$DISPLAY \	
 	--cap-add=NET_ADMIN \
 	--device=/dev/net/tun \
-	--name poky poky:latest
+	--name poky gcamerli/poky:latest
